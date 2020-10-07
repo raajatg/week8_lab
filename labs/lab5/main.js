c0aec8172d72e3df1e2e9191c3430ebab13d7fc8
horvathaa <ahorvath@andrew.cmu.edu>
2020-10-06 21:50:19 -0400

    finished

--- labs/lab5/main.js
+++ labs/lab5/main.js
@@ -1,9 +1,26 @@
-function addNewList() {
-    alert('hello world!');
-    console.log('hello world!');
-}
-
 addNewList = () => {
     alert('hello world with arrow!');
     console.log('hello world!');
+}
+
+addListItem = () => {
+    const list = document.getElementById('grocery-list');
+    const inputField = document.getElementById('new-list-item');
+    const newItem = document.createElement('li');
+    newItem.appendChild(document.createTextNode(inputField.value));
+    list.appendChild(newItem);
+}
+
+deleteListItem = (element) => {
+    element.parentNode.removeChild(element);
+}
+
+completeListItem = (element) => {
+    console.log(element);
+    if(element.checked) {
+        element.parentNode.style.textDecoration = "line-through";
+    }
+    else {
+        element.parentNode.style.textDecoration = "none";
+    }
 }
