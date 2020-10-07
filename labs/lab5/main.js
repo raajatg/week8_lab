0803b5e7778b6c6a409bf58eba9ef9e62abe85b0
horvathaa <ahorvath@andrew.cmu.edu>
2020-10-06 21:24:18 -0400

    pushing my dumb button

---
+++ labs/lab5/main.js
@@ -0,0 +1,9 @@
+function addNewList() {
+    alert('hello world!');
+    console.log('hello world!');
+}
+
+addNewList = () => {
+    alert('hello world with arrow!');
+    console.log('hello world!');
+}
